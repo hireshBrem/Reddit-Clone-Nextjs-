c6434a154758b5d21c59a69cffe1267550efffd2
Hiresh <hbremanand4@gmail.com>
2023-07-04 14:31:39 +0100

    eslint error

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,6 +1,6 @@
 'use client' // Error components must be Client Components
  
-import { useEffect } from 'react'
+import { useEffect } from 'react';
  
 export default function Error({
   error,
@@ -23,14 +23,6 @@ export default function Error({
                 <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">Something's missing.</p>
                 <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">Sorry, we can't find that page. You'll find lots to explore on the home page. </p>
                 <a href="/" className="inline-flex text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4">Back to Homepage</a>
-                <button className='flex justify-center'
-                    onClick={
-                    // Attempt to recover by trying to re-render the segment
-                    () => reset()
-                    }
-                >
-                    Try again
-                </button>
             </div>   
         </div>
     </section>
