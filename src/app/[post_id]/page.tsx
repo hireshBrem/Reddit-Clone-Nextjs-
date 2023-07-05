98736e78dc9d428c2caa6ca3a017f50468bc3ace
Hiresh <hbremanand4@gmail.com>
2023-07-05 17:33:45 +0100

    f

--- src/app/[post_id]/page.tsx
+++ src/app/[post_id]/page.tsx
@@ -24,12 +24,12 @@ export default async function Page({params,searchParams}: {params: { post_id: st
     <div className='m-2 flex-1 flex flex-col'>
         <div className='flex flex-row w-full'>
             <div className='dark:bg-[#272729] border-r-2 dark:border-black  flex flex-col align-middle w-10 bg-[#F6F7F8] rounded-l-md'>               
-                {/* <VoteButton post_id={post_id} votes={post.post_votes} changeVotes={changeVotes} vote="up" />
+                <VoteButton post_id={post_id} votes={post.post_votes} changeVotes={changeVotes} vote="up" />
                 <h1 className='mx-4'>{post.post_votes}</h1>
                 <VoteButton post_id={post_id} votes={post.post_votes} changeVotes={changeVotes} vote="down" />
-                <Button p={searchParams.name} /> */}
+                <Button p={searchParams.name} />
             </div>
-            <div className='dark:bg-[#272729] w-[100%] border-b-2 border-gray-200 space-y-2 flex flex-col p-2 bg-white rounded-r-md'>
+            {/* <div className='dark:bg-[#272729] w-[100%] border-b-2 border-gray-200 space-y-2 flex flex-col p-2 bg-white rounded-r-md'>
                 <div className='text-sm flex space-x-5'>
                     <h1>Posted by u/{name}</h1><h1>   {searchParams.post_date}</h1>
                 </div>
@@ -39,7 +39,7 @@ export default async function Page({params,searchParams}: {params: { post_id: st
                 <div>
                     <h1>{post.post_text}</h1>
                 </div>
-                <div>
+            <div> */}
             {
                 (comments) ?
                 comments.map(async(comment, index:number) => {
