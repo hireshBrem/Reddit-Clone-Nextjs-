de226facd6a5043bb6b7261ccea2d3d6b8b926e4
Hiresh <hbremanand4@gmail.com>
2023-07-05 11:01:20 +0100

    h

--- src/app/[post_id]/page.tsx
+++ src/app/[post_id]/page.tsx
@@ -12,9 +12,9 @@ export default async function Page({params,searchParams}: {params: { post_id: st
     let post_id = searchParams.post_id
     console.log(name + " " + post_id)
 
-    let post = await getPostDetail(post_id)
-    let comments = await getComments(post_id)
-    let post_date = new Date(post.post_date).toLocaleDateString()
+    // let post = await getPostDetail(post_id)
+    // let comments = await getComments(post_id)
+    // let post_date = new Date(post.post_date).toLocaleDateString()
 
     
     return(
@@ -22,7 +22,7 @@ export default async function Page({params,searchParams}: {params: { post_id: st
     <div className='max-w-5xl m-auto flex flex-row justify-center pt-5'>
     <div className='m-2 flex-1 flex flex-col'>
         <div className='flex flex-row w-full'>
-            <div className='dark:bg-[#272729] border-r-2 dark:border-black  flex flex-col align-middle w-10 bg-[#F6F7F8] rounded-l-md'>               
+            {/* <div className='dark:bg-[#272729] border-r-2 dark:border-black  flex flex-col align-middle w-10 bg-[#F6F7F8] rounded-l-md'>               
                 <VoteButton post_id={post_id} votes={post.post_votes} changeVotes={changeVotes} vote="up" />
                 <h1 className='mx-4'>{post.post_votes}</h1>
                 <VoteButton post_id={post_id} votes={post.post_votes} changeVotes={changeVotes} vote="down" />
@@ -37,32 +37,32 @@ export default async function Page({params,searchParams}: {params: { post_id: st
                 <div>
                     <h1>{post.post_text}</h1>
                 </div>
-                <div>
+                <div> */}
             {
-                (comments) ?
-                comments.map(async(comment, index:number) => {
-                    let profile_photo = await getUserDetail(comment.user_id, "profile_photo")
-                    let name = await getUserDetail(comment.user_id, "name")
+                // (comments) ?
+                // comments.map(async(comment, index:number) => {
+                //     let profile_photo = await getUserDetail(comment.user_id, "profile_photo")
+                //     let name = await getUserDetail(comment.user_id, "name")
 
-                    let comment_date = new Date(comment.date).toLocaleDateString()
+                //     let comment_date = new Date(comment.date).toLocaleDateString()
 
-                    return(
-                        <div key={index} className="border-t-2 my-5 border-grayy-300">
-                            <Image className="inline-block rounded-md mt-2" src={profile_photo} alt="pic" width={30} height={30} />
-                            <h1 className="inline-block ml-5">u/{name} {comment_date}</h1>
-                            <h1>{comment.content}</h1>
-                        </div>
-                    )
-                })
-                :null
+                //     return(
+                //         <div key={index} className="border-t-2 my-5 border-grayy-300">
+                //             <Image className="inline-block rounded-md mt-2" src={profile_photo} alt="pic" width={30} height={30} />
+                //             <h1 className="inline-block ml-5">u/{name} {comment_date}</h1>
+                //             <h1>{comment.content}</h1>
+                //         </div>
+                //     )
+                // })
+                // :null
             }                
                 <CommentBox post_id={post_id} />
             </div>
             </div>
         </div>
-    </div>
-    <Sidebar />
-    </div>
+    // </div>
+    // <Sidebar />
+    // </div>
     :null
     )
 }
